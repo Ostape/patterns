a5ed1352d366f5dfec8481fb1011f59ec8800952
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-04-11 17:33:53 +0300

    add BacteriaFactory

---
+++ pattern/src/main/java/com/robosh/BacteriaFactory.java
@@ -0,0 +1,34 @@
+package com.robosh;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BacteriaFactory {
+    private static final Map<String, Bacteria> bacterias = new HashMap<String, Bacteria>();
+
+    public Bacteria getBacteria(String typeBacteria){
+        Bacteria bacteria = bacterias.get(typeBacteria);
+
+        if (bacteria == null){
+            switch (typeBacteria){
+                case "Actinobacteria":
+                    bacteria = new Actinobacteria();
+                    break;
+
+                case "Chloroflexi":
+                    bacteria = new Chloroflexi();
+                    break;
+
+                case "Firmicutes":
+                    bacteria = new Firmicutes();
+
+                case "Spirochaetes":
+                    bacteria = new Spirochaetes();
+                    break;
+            }
+            bacterias.put(typeBacteria, bacteria);
+        }
+        return bacteria;
+    }
+
+}
